959b2a4a4b04214d8c4d55f2ffdd16d379efd869
Kyle Frost <kylefrost@users.noreply.github.com>
2017-07-21 11:07:32 -0400

    Remove Vapor dependencies from Package.swift

--- Package.swift
+++ Package.swift
@@ -5,8 +5,6 @@ import PackageDescription
 let package = Package(
     name: "Hedwig",
     dependencies: [
-        .Package(url: "https://github.com/vapor/tls.git", majorVersion: 1),
-        .Package(url: "https://github.com/vapor/crypto.git", majorVersion: 1),
         .Package(url: "https://github.com/onevcat/AddressParser.git", majorVersion: 1),
         .Package(url: "https://github.com/onevcat/MimeType.git",  majorVersion: 1)
     ]
